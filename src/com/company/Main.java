2beb4b179a9cded8180c5090e21e366fc63ddf80
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2021-10-04 13:52:51 +0500

    Домашняя работа №2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("expression: " + expression(1,2,3,4));
-        System.out.println("checkRange 10,2: " + checkRange(10,2));
-        System.out.println("isPositive 10: " + isPositive(10));
-        System.out.println("isNegative 2: " + isNegative(2));
-        System.out.println("isLeapYear 10: " + isLeapYear(10));
-        System.out.println("isLeapYear 12: " + isLeapYear(12));
-        System.out.println("isLeapYear 100: " + isLeapYear(100));
-        System.out.println("isLeapYear 1200: " + isLeapYear(1200));
-        greeting("Bob");
-    }
-    static double expression(float a,float b,float c,float d) {
-        return a*(b+(c/d));
-    }
-    static boolean checkRange(float a, float b) {
-        float sum = a + b;
-        return sum > 10 && sum < 20;
-    }
-    static boolean isPositive(int a) {
-        return a >= 0;
-    }
-    static boolean isNegative(int a) {
-        return a <= 0;
-    }
-    static void greeting(String name) {
-        System.out.println("Привет, " + name);
-    }
-    static boolean isLeapYear(int year) {
-        if(year % 4 != 0 ) {
-            return false;
-        }
-        return year % 100 != 0 || year % 400 == 0;
+        HomeWork1 homeWork1 = new HomeWork1();
+        HomeWork2 homeWork2 = new HomeWork2();
+        // homeWork1
+        System.out.println("expression: " + homeWork1.expression(1,2,3,4));
+        System.out.println("checkRange 10,2: " + homeWork1.checkRange(10,2));
+        System.out.println("isPositive 10: " + homeWork1.isPositive(10));
+        System.out.println("isNegative 2: " + homeWork1.isNegative(2));
+        System.out.println("isLeapYear 10: " + homeWork1.isLeapYear(10));
+        System.out.println("isLeapYear 12: " + homeWork1.isLeapYear(12));
+        System.out.println("isLeapYear 100: " + homeWork1.isLeapYear(100));
+        System.out.println("isLeapYear 1200: " + homeWork1.isLeapYear(1200));
+        homeWork1.greeting("Bob");
+        homeWork2.task1();
+        homeWork2.task2();
+        homeWork2.task3();
+        homeWork2.task4();
+        homeWork2.task5();
+        homeWork2.task6(new int[] {1,1,1,2,1});
     }
 }
